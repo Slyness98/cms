15b59f44f7952cdd3600d6587e49b0b458a57d14
Slyness98 <sethlyness@outlook.com>
2019-02-27 10:23:13 -0800

    Initial Commit

---
+++ includes/db.php
@@ -0,0 +1,16 @@
+<?php
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
+//db array of constants being defined. More secure way of passing db keys through mysqli_connect.
+foreach ($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER,DB_PASS, DB_NAME);
+if($connection){
+	echo "connected to cms";
+}
+
+?>
