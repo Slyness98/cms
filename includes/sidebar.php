351ce922dc26fc616e8ad5826d24c4038c5eec76
Slyness98 <sethlyness@outlook.com>
2019-03-02 16:54:44 -0800

    small cleanup of admin index page and its components.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,53 +1,49 @@
 
  <div class="col-md-4">
 
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
 
-                <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
+	    <!-- Blog Search Well -->
+	    <div class="well">
+	        <h4>Blog Search</h4>
+	        <form action="search.php" method="post">
+	        <div class="input-group">
+	            <input name="search" type="text" class="form-control">
+	            <span class="input-group-btn">
+	                <button name="submit" class="btn btn-default" type="submit">
+	                    <span class="glyphicon glyphicon-search"></span>
+	            </button>
+	            </span>
+	        </div>
+	        <!-- /.input-group -->
+	    </form><!-- search from -->
+	    </div>
+
+	     <!-- Blog Categories Well -->
+<?php
+ $query = "SELECT * FROM categories";
+    $select_all = mysqli_query($connection,$query);
+?>
+               
+        <div class="well">
+            <h4>Blog Categories</h4>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-unstyled">
+
+ <?php  
+ 	 while ($row = mysqli_fetch_assoc($select_all)) {
+     $cat_title = $row['cat_title'];
+     echo "<li><a href='#''>{$cat_title}</a></li>";
+    }
+ ?>
+                             
+                    </ul>
                 </div>
+                <!-- /.col-lg-12 -->
+          
+            </div>
+            <!-- /.row -->
+        </div>
 
                 <!-- Side Widget Well -->
                 <div class="well">
--- index.php
+++ index.php
@@ -9,38 +9,44 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+            <h1 class="page-header">
+                Latest Posts
+             <small>Secondary Text</small>
+            </h1>
+            <?php  
+                
+                $query = "SELECT * FROM posts";
+                $query_all_posts = mysqli_query($connection,$query);
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                while ($row = mysqli_fetch_assoc($query_all_posts)) {
+                 $post_title = $row['post_title'];
+                 $post_author = $row['post_author'];
+                 $post_date = $row['post_date'];
+                 $post_image = $row['post_image'];
+                 $post_content = $row['post_content'];
+            ?>
+            <!-- Blog Post Structure -->
+            <h2>
+                <a href="#"><?php echo $post_title; ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="about.php"> <?php echo $post_author ?> </a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
+            <hr>
+            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="featured post image">
+            <hr>
+            <p><?php echo $post_content; ?></p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-               
-
+            <hr>
+            <?php }  //closes off the while loop querying all post information. The HTML code is now automatically replecated for each new post by being encased in this loop.  ?>
             </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-           <?php include "includes/sidebar.php"; ?>
-
-        </div>
-        <!-- /.row -->
-
-        <hr>
-<?php include 'includes/footer.php'; ?>
         
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/sidebar.php"; ?>
+          </div>
+            <!-- /.row -->
+ <hr>
+<?php include 'includes/footer.php'; ?>
+ <!--footer.php includes closing div for <div class="container">. I felt it was better to only have the seemingly unclosed "container" class instance twice (once in search.php and once in index.php) than to open the class at the end of header.php across the entire CMS, should I ever need to change classes of the outermost div for a certain page. It's best to end header.php with the start of the <body> tag.        
